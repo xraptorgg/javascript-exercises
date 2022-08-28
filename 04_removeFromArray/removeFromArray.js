e3e4f20e6789a3a16711e339ae897a046ed69ee7
xRaptorGG <x.shayan.halder@gmail.com>
2022-08-28 16:45:24 +0530

    Solved problems 1 through 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+    const args = arg;
+    for (let i = 0; i < args.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (args[i] === array[j]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+    return array;
 
 };
 
